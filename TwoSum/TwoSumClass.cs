c59abc68c515c0bfcd993f8f0b48abfeb06d52ff
Ryan Robinson <274507+Veranith@users.noreply.github.com>
2021-10-06 17:39:26 -0600

    Adds TwoSum method

--- TwoSum/TwoSumClass.cs
+++ TwoSum/TwoSumClass.cs
@@ -10,7 +10,17 @@ namespace TwoSumProject
     {
         public static int[] TwoSum(int[] nums, int target)
         {
-            return new int[] { 0, 1 };
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
         }
     }
 }
